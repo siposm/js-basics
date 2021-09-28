6dd5aa3858ae83eff5de7f172a77c9c1e5209361
Sipos Miklos <commit@siposm.hu>
2021-09-28 13:07:20 +0200

    added cat module to user

--- user.js
+++ user.js
@@ -1,6 +1,17 @@
+import Cat from './cat.js'
+
 class User {
-    userName = "";
-    userAge = "";
+    userName = ""
+    userAge = ""
+    cat = undefined
+
+    constructor() {
+        this.userName = "lorem"
+        this.userAge = 0
+        this.cat = new Cat()
+        this.cat.catName = "lorem-cat"
+        this.cat.catAge = 0
+    }
 
     printName () {
         console.log(userName)
