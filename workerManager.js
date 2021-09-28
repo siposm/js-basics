1d8e38ffd83d59711f882a8bac064240a97a5cf6
Sipos Miklos <commit@siposm.hu>
2021-09-28 16:14:35 +0200

    added template version

---
+++ workerManager.js
@@ -0,0 +1,33 @@
+
+let workerArray = []
+
+// https://stackoverflow.com/questions/807878/how-to-make-javascript-execute-after-page-load?rq=1
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    fetch('https://reqres.in/api/users')
+        .then(res => res.json()) // console log res
+        .then(data => {
+            for (let i = 0; i < data.data.length; i++) {
+                workerArray.push(data.data[i])
+            }
+        }).then( () => {
+
+            console.log(workerArray)
+
+            // html template should be supported : https://developer.mozilla.org/en-US/docs/Web/HTML/Element/template
+
+            for (let i = 0; i < workerArray.length; i++) {
+                let template = document.getElementById("template").content
+                let copyHTML = document.importNode(template,true)
+                copyHTML.querySelector(".worker-name").textContent = workerArray[i].first_name + " " + workerArray[i].last_name
+                copyHTML.querySelector(".worker-email").textContent = workerArray[i].email
+                document.getElementById("worker-container").appendChild(copyHTML)
+            }
+
+
+        })
+
+
+})
